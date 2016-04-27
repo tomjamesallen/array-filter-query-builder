6e258ef9f016c69707ecc3807bed7fcc4169557a
Tom Allen <tomjamesallenweb@gmail.com>
2016-04-27 11:03:45 +0100

    Building out API methods.

--- src/QueryBuilder.js
+++ src/QueryBuilder.js
@@ -1,25 +1,65 @@
+import clone from 'clone'
+
 export default class QueryBuilder {
-  constructor() {
-    
+  constructor(importObject) {
+    this._currentQuery = {}
+    this.deserialise(importObject)
   }
 
-  update(key, filter) {
-
+  update(keyInput, arg2 = false, arg3 = false) {
+    if (typeof keyInput === 'string') {
+      let queryArgument = arg2
+      let reset = arg3
+      if (queryArgument) {
+        this._currentQuery[keyInput] = queryArgument
+      }
+    }
+    else if (typeof keyInput === 'object') {
+      let reset = arg2
+      if (reset) {
+        this.reset()
+      }
+      this.deserialise(keyInput)
+    }
+    return this
   }
 
-  reset(key = null) {
-
+  reset(keyInput = null) {
+    if (keyInput === null) {
+      this._currentQuery = {}
+    }
+    else if (Array.isArray(keyInput)) {
+      let keys = keyInput
+      keys.forEach((key) => {
+        delete this._currentQuery[key]
+      })
+    }
+    else if (typeof keyInput === 'string') {
+      let key = keyInput
+      delete this._currentQuery[key]
+    }
+    return this
   }
 
   deserialise(importObject) {
-
-  }
-
-  clone() {
-
+    if (typeof importObject === 'object') {
+      const keys = Object.keys(importObject)
+      keys.forEach((key) => {
+        this._currentQuery[key] = importObject[key]
+      })
+    }
+    return this
   }
 
   serialise() {
+    let serialised = {}
+    Object.keys(this._currentQuery).forEach((key) => {
+      serialised[key] = this._currentQuery[key]
+    })
+    return serialised
+  }
 
+  clone() {
+    return clone(this)
   }
 }
