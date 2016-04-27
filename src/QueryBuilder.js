fb649f065695ba7e2b20b69707b27eeadcc8f9f1
Tom Allen <tomjamesallenweb@gmail.com>
2016-04-27 15:28:58 +0100

    Working on tests and functionality for `add` and `replace` methods in `QueryBuilder`.

--- src/QueryBuilder.js
+++ src/QueryBuilder.js
@@ -6,24 +6,6 @@ export default class QueryBuilder {
     this.deserialise(importArray)
   }
 
-  // update(keyInput, arg2 = false, arg3 = false) {
-  //   if (typeof keyInput === 'string') {
-  //     let queryArgument = arg2
-  //     let reset = arg3
-  //     if (queryArgument) {
-  //       this._currentQuery[keyInput] = queryArgument
-  //     }
-  //   }
-  //   else if (typeof keyInput === 'object') {
-  //     let reset = arg2
-  //     if (reset) {
-  //       this.reset()
-  //     }
-  //     this.deserialise(keyInput)
-  //   }
-  //   return this
-  // }
-
   // reset(keyInput = null) {
   //   if (keyInput === null) {
   //     this._currentQuery = {}
@@ -41,16 +23,62 @@ export default class QueryBuilder {
   //   return this
   // }
 
-  add() {
-    
+  _addItemToCurrentQuery(fieldKey, comparator, value) {
+    const queryArgument = [fieldKey, comparator, value]
+    this._currentQuery.push(queryArgument)
+  }
+
+  _removeKeyFromCurrentQuery(fieldKey) {
+    const updatedQuery = this._currentQuery.filter((item) => {
+      if (item[0] === fieldKey) return false
+      else return true
+    })
+    this._currentQuery = updatedQuery
+  }
+
+  add(keyInput, arg2 = false, arg3 = false) {
+    if (typeof keyInput === 'string') {
+      if (!arg2) return this
+
+      const reset = arg3
+      const fieldKey = keyInput
+      const queryArguments = arg2
+
+      if (reset) this._removeKeyFromCurrentQuery(fieldKey)
+
+      Object.keys(queryArguments).forEach((comparator) => {
+        const value = queryArguments[comparator]
+        this._addItemToCurrentQuery(fieldKey, comparator, value)
+      })
+    }
+    else if (Array.isArray(keyInput)) {
+      const queryArguments = keyInput
+      const reset = arg2
+
+      queryArguments.forEach((queryArgument) => {
+        if (reset) this._removeKeyFromCurrentQuery(queryArgument[0])
+        this._addItemToCurrentQuery(...queryArgument)
+      })
+    }
+
+    return this
+  }
+
+  replace(keyInput, arg2 = false) {
+    if (typeof keyInput === 'string') {
+      if (arg2) {
+        this.add(keyInput, arg2, true)
+      }
+    }
+    else if (Array.isArray(keyInput)) {
+      this.add(keyInput, true)
+    }
+
+    return this
   }
 
   reset() {
     
-  }
-
-  replace() {
-
   }
 
   deserialise(importArray) {
--- src/test/basicQueryChecks.js
+++ src/test/basicQueryChecks.js
@@ -23,14 +23,95 @@ describe('New `Query`s should accept a serialised array of query arguments', () 
   })
 })
 
-describe('A `Query`s `add` method should accept a `keyInput`+`queryArgument` pair or an array of query arguments', () => {
-  it('accepts a `keyInput`+`queryArgument` pair', () => {
+describe('A `Query`s `add` method should accept a `fieldKey`+`queryArgument` pair or an array of query arguments', () => {
+  it('should accept a `fieldKey`+`queryArgument` pair', () => {
     let query = new QueryBuilder(dummyQuery1)
-    query.add('numericTestField', {
+    query.add('numericTestFieldKey', {
       is: 3
     })
     const serialised = query.serialise()
     expect(serialised.length).to.equal(dummyQuery1.length + 1)
+  })
+
+  it('should accept a `fieldKey`+`queryArgument` pair with multiple queryArguments per `fieldKey`', () => {
+    let query = new QueryBuilder(dummyQuery1)
+    query.add('numericTestField', {
+      is: 3,
+      isNot: 4
+    })
+    const serialised = query.serialise()
+    expect(serialised.length).to.equal(dummyQuery1.length + 2)
+  })
+
+  it('should accept a boolean reset value as a third argument, which resets any query arguments matching the field.', () => {
+    let query = new QueryBuilder(dummyQuery1)
+    query.add('boolTestField', {
+      is: false
+    }, true)
+    const serialised = query.serialise()
+    expect(serialised.length).to.equal(dummyQuery1.length)
+  })
+})
+
+describe('A `Query`s `add` method should accept an array of query arguments', () => {
+  it('should append the query arguments and return all of the arguments from its `serialise` method', () => {
+    let query = new QueryBuilder(dummyQuery1)
+    query.add([
+      ['boolTestField', 'is', false],
+      ['numericTestField', 'is', 4]
+    ])
+    const serialised = query.serialise()
+    expect(serialised.length).to.equal(dummyQuery1.length + 2)
+  })
+
+  it('should accept a boolean reset value as a second argument, which will reset any query arguments matching the field of any of the query arguments.', () => {
+    let query = new QueryBuilder(dummyQuery1)
+    query.add([
+      ['boolTestField', 'is', false],
+      ['numericTestField', 'is', 4]
+    ], true)
+    const serialised = query.serialise()
+    expect(serialised.length).to.equal(dummyQuery1.length + 1)
+  })
+})
+
+describe('A `Query`s `replace` method should behave in the same way as the `add` method, but with `reset` set to true.', () => {
+  it('should delete any existing query arguments with the same `fieldKey` and then add the new query argument', () => {
+    let query = new QueryBuilder(dummyQuery1)
+    query.replace('boolTestField', {
+      is: false
+    })
+    const serialised = query.serialise()
+    const lastQueryArgument = serialised[serialised.length - 1]
+    expect(serialised.length).to.equal(dummyQuery1.length)
+    expect(shallowequal(lastQueryArgument, ['boolTestField', 'is', false])).to.equal(true)
+  })
+
+  it('should accept an array of query arguments, and delete any existing query arguments that match the `fieldKey`s.', () => {
+    let query = new QueryBuilder(dummyQuery1)
+    query.replace(dummyQuery2)
+    const serialised = query.serialise()
+    const start = serialised.length - dummyQuery2.length
+    const end = serialised.length
+    const newItems = serialised.slice(start, end)
+
+    let isEqual = true
+
+    dummyQuery2.forEach((queryArgument, i) => {
+      const newItemsQueryArgument = newItems[i]
+      if (typeof newItemsQueryArgument !== 'object') {
+        isEqual = false
+        return
+      }
+      queryArgument.forEach((arg, i) => {
+        const newItemsQueryArgumentArg = newItemsQueryArgument[i]
+        if (arg !== newItemsQueryArgumentArg) {
+          isEqual = false
+        }
+      })
+    })
+
+    expect(isEqual).to.equal(true)
   })
 })
 
