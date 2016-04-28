fc9d5282e8767c77c4e6134355672e0ab846cccb
Tom Allen <tomjamesallenweb@gmail.com>
2016-04-28 14:41:13 +0100

    Updating fixture items to include an object item for the purposes of testing `shallowequals`.

--- src/fixtures/items1.js
+++ src/fixtures/items1.js
@@ -3,18 +3,30 @@ export default [
     name: 'Product 1',
     boolTestField: true,
     valueTestField: 'first',
-    numericTestField: 0
+    numericTestField: 0,
+    objectWithKeys: {
+      key1: 0,
+      key2: 1
+    }
   },
   {
     name: 'Product 2',
     boolTestField: false,
     valueTestField: 'second',
-    numericTestField: 5
+    numericTestField: 5,
+    objectWithKeys: {
+      key1: 2,
+      key2: 3
+    }
   },
   {
     name: 'Product 3',
     boolTestField: false,
     valueTestField: 'third',
-    numericTestField: 10
+    numericTestField: 10,
+    objectWithKeys: {
+      key1: 4,
+      key2: 5
+    }
   }
 ]
