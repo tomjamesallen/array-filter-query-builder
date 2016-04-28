c59b6d2be34d24765cb3648616fd50bb3e645d6a
Tom Allen <tomjamesallenweb@gmail.com>
2016-04-28 12:32:56 +0100

    Writing test for `is` comparator.

--- src/test/comparatorMethodTests.js
+++ src/test/comparatorMethodTests.js
@@ -2,3 +2,50 @@
 import { expect } from 'chai'
 
 import { QueryBuilder, Filter } from '../index'
+
+import dummyItems1 from '../fixtures/items1'
+
+describe('Comparator methods', () => {
+  describe('#is', () => {
+    it('should', () => {
+      const filter = new Filter()
+      const query = new QueryBuilder('boolTestField', {
+        is: true
+      })
+      const filtered = filter.run(dummyItems1, query)
+      expect(filtered.length).to.equal(1)
+    })
+  })
+
+  describe('#isShallowEqual', () => {
+
+  })
+
+  describe('#isNot', () => {
+    
+  })
+
+  describe('#isAtLeast', () => {
+    
+  })
+
+  describe('#isLessThanOrEqualTo', () => {
+    
+  })
+
+  describe('#isMoreThan', () => {
+    
+  })
+
+  describe('#isLessThan', () => {
+    
+  })
+
+  describe('#isOneOf', () => {
+    
+  })
+
+  describe('custom comparator', () => {
+
+  })
+})
