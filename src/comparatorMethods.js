7fedc755906f6d064f8b1162349f62d434f89f6b
Tom Allen <tomjamesallenweb@gmail.com>
2016-04-28 14:43:35 +0100

    Adding comparator methods to pass tests. Not all methods written yet.

--- src/comparatorMethods.js
+++ src/comparatorMethods.js
@@ -1,9 +1,37 @@
+import shallowequal from 'shallowequal'
+
 export default {
   is(val1, val2) {
     return val1 === val2
+  },
+  isNot(val1, val2) {
+    return val1 !== val2
+  },
+  isShallowEqual(val1, val2) {
+    return shallowequal(val1, val2)
+  },
+  isNotShallowEqual(val1, val2) {
+    return !shallowequal(val1, val2)
+  },
+  isOneOf(val1, arrayOfTests) {
+    if (!Array.isArray(arrayOfTests)) {
+      let val2 = arrayOfTests
+      return this.is(val1, val2)
+    }
+    else if (!arrayOfTests.length) {
+      return true
+    }
+    else {
+      let hasMatch = false
+      arrayOfTests.forEach((test) => {
+        if (val1 === test) {
+          hasMatch = true
+        }
+      })
+      return hasMatch
+    }
   }
   // ,
   // isAtLeast(input) {},
   // isOneOf(input) {},
-  // isNot(input) {}
 }
