e4380e68ab28917338c7208cfd5670156de7173f
Tom Allen <tomjamesallenweb@gmail.com>
2016-05-03 14:38:06 +0100

    Explicitly converting numbers to strings for `is` comparator, and strings to floats for numeric comparators.

--- src/comparatorMethods.js
+++ src/comparatorMethods.js
@@ -1,11 +1,25 @@
 import shallowequal from 'shallowequal'
 
+function convertNumberToString(val) {
+  if (typeof val === 'number') {
+    val = val.toString()
+  }
+  return val
+}
+
+function convertStringToNumber(val) {
+  if (typeof val === 'string') {
+    val = parseFloat(val)
+  }
+  return val
+}
+
 export default {
   is(val1, val2) {
-    return val1 === val2
+    return convertNumberToString(val1) === convertNumberToString(val2)
   },
   isNot(val1, val2) {
-    return val1 !== val2
+    return convertNumberToString(val1) !== convertNumberToString(val2)
   },
   isShallowEqual(val1, val2) {
     return shallowequal(val1, val2)
@@ -24,7 +38,7 @@ export default {
     else {
       let hasMatch = false
       arrayOfTests.forEach((test) => {
-        if (val1 === test) {
+        if (this.is(val1, test)) {
           hasMatch = true
         }
       })
@@ -48,15 +62,15 @@ export default {
     return hasMatch
   },
   isAtLeast(val1, val2) {
-    return val1 >= val2
+    return convertStringToNumber(val1) >= convertStringToNumber(val2)
   },
   isLessThanOrEqualTo(val1, val2) {
-    return val1 <= val2
+    return convertStringToNumber(val1) <= convertStringToNumber(val2)
   },
   isMoreThan(val1, val2) {
-    return val1 > val2
+    return convertStringToNumber(val1) > convertStringToNumber(val2)
   },
   isLessThan(val1, val2) {
-    return val1 < val2
+    return convertStringToNumber(val1) < convertStringToNumber(val2)
   }
 }
